5246e13eb19f063b8310e031ecc2902b43b40574
aabril86 <aabril@elpuig.xeil.net>
2020-10-15 01:18:50 +0200

    w/tabs

--- app/src/main/java/com/example/p3navigationex/Tabbed1Fragment.java
+++ app/src/main/java/com/example/p3navigationex/Tabbed1Fragment.java
@@ -1,63 +1,75 @@
 package com.example.p3navigationex;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.p3navigationex.databinding.FragmentTabbed1Binding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Tabbed1Fragment#newInstance} factory method to
+ * Use the {@link Tabbed1Fragment#} factory method to
  * create an instance of this fragment.
  *
  */
 public class Tabbed1Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentTabbed1Binding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tabbed1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Tabbed1Fragment newInstance(String param1, String param2) {
-        Tabbed1Fragment fragment = new Tabbed1Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public Tabbed1Fragment() {
-        // Required empty public constructor
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentTabbed1Binding.inflate(inflater, container, false)).getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabbed1, container, false);
+        binding.viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                switch (position) {
+                    case 0: default:
+                        return new Tabbed1AFragment();
+                    case 1:
+                        return new Tabbed1BFragment();
+                    case 2:
+                        return new Tabbed1CFragment();
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return 3;
+            }
+        });
+
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0: default:
+                        tab.setText("TAB A");
+                        break;
+                    case 1:
+                        tab.setText("TAB B");
+                        break;
+                    case 2:
+                        tab.setText("TAB C");
+                        break;
+                }
+            }
+        }).attach();
+
     }
 }
